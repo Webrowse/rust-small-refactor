932796251835f5cfc8f801d467cce28139738d4c
Webrowse <great.adarsh@gmail.com>
2026-01-14 23:49:10 +0530

    initial code

---
+++ src/main.rs
@@ -0,0 +1,30 @@
+use std::env;
+use std::fs;
+
+fn main() {
+    let args: Vec<String> = env::args().collect();
+
+    if args.len() < 2 {
+        println!("usage: word_count <file>");
+        return;
+    }
+
+    let path = &args[1];
+    let contents = fs::read_to_string(path).unwrap();
+
+    let count = count_words(&contents);
+
+    println!("word count: {}", count);
+}
+
+fn count_words(input: &str) -> usize {
+    let mut count = 0;
+
+    for word in input.split(" ") {
+        if word.len() > 0 {
+            count += 1;
+        }
+    }
+
+    count
+}
